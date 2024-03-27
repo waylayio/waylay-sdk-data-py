2ec7968482c4a2cdcab84556d0aa57802623ecb4
Simon Schoonjans <simon.schoonjans@waylay.io>
2024-03-27 15:39:20 +0100

    chore: regenerate service (#6)

--- waylay-sdk-data/src/waylay/services/data/api/events_api.py
+++ waylay-sdk-data/src/waylay/services/data/api/events_api.py
@@ -264,7 +264,7 @@ class EventsApi(WithApiClient):
             **body_args,
             headers=headers,
             **kwargs,
-            response_types_map=response_types_map,
+            response_type=response_types_map,
             select_path=select_path,
             raw_response=raw_response,
         )
@@ -441,7 +441,7 @@ class EventsApi(WithApiClient):
             **body_args,
             headers=headers,
             **kwargs,
-            response_types_map=response_types_map,
+            response_type=response_types_map,
             select_path=select_path,
             raw_response=raw_response,
         )
@@ -606,7 +606,7 @@ class EventsApi(WithApiClient):
             **body_args,
             headers=headers,
             **kwargs,
-            response_types_map=response_types_map,
+            response_type=response_types_map,
             select_path=select_path,
             raw_response=raw_response,
         )
@@ -765,7 +765,7 @@ class EventsApi(WithApiClient):
             **body_args,
             headers=headers,
             **kwargs,
-            response_types_map=response_types_map,
+            response_type=response_types_map,
             select_path=select_path,
             raw_response=raw_response,
         )
